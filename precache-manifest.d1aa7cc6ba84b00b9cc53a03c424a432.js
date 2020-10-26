7f85268d33741bd2ab3d41dd7a8a78e36c97477a
Xavier Ho <xavier@jtg.design>
2020-10-26 11:49:14 +1100

    Updates

---
+++ precache-manifest.d1aa7cc6ba84b00b9cc53a03c424a432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de15c5b10c7bd837c775580b34e11f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f1077bf5d0275deacef",
+    "url": "/static/css/main.1673ff74.chunk.css"
+  },
+  {
+    "revision": "2e19b471e2af7bd1ca6b",
+    "url": "/static/js/2.f5df116d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.f5df116d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1077bf5d0275deacef",
+    "url": "/static/js/main.f5c0b84f.chunk.js"
+  },
+  {
+    "revision": "7c39cb6d82c4279f8920",
+    "url": "/static/js/runtime-main.1af4cc07.js"
+  }
+]);
